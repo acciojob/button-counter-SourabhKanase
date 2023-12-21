843dff366857caeaa5ee38c73256647ff21d1e2d
SourabhKanase <141937826+SourabhKanase@users.noreply.github.com>
2023-12-21 11:35:49 +0000

    newchanges

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [value,setvalue]=useState(0);
+  function increase()
+  {
+    console.log(value);
+    setvalue(value+1);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>Button clicked {value} times</p>
+        <button onClick={increase}>Click me</button>
     </div>
   )
 }
